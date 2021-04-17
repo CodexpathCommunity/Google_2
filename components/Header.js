56b30c28be3d70523e38b52af2f50bfefc45a9fc
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-17 15:35:35 +0100

    done with the header for search page

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef } from "react";
+import Avatar from "./Avatar";
 
 function Header() {
   const router = useRouter();
@@ -11,35 +12,41 @@ function Header() {
     e.preventDefault();
     const term = searchInputRef.current.value;
     if (!term) return;
-    router.push(`/search?term={term}`);
+    router.push(`/search?term=${term}`);
   };
   return (
-    <header>
-      <Image
-        src="/images/ggle.png"
-        height={40}
-        width={120}
-        className="cursor-pointer"
-        onClick={() => router.push("/")}
-      />
-      <form className="flex border px-6 py-3 ml-10 mr-5 border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-        <input
-          type="text"
-          ref={searchInputRef}
-          className="flex-grow w-full focus:outline-none"
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="/images/ggle.png"
+          height={40}
+          width={120}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
         />
-        <XIcon
-          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
-          onClick={() => (searchInputRef.current.value = "")}
-        />
-        <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
-        <SearchIcon
-          className="h-6 hidden sm:inline-flex text-blue-500 "
-          onClick={search}
-        />
+        <form className="flex flex-grow border px-6 py-3 ml-10 mr-5 border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            type="text"
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon
+            className="h-6 hidden sm:inline-flex text-blue-500 cursor-pointer"
+            onClick={search}
+          />
 
-        <button hidden type="submit" onClick={search}></button>
-      </form>
+          <button hidden type="submit" onClick={search}></button>
+        </form>
+        <Avatar
+          url="https://avatars.githubusercontent.com/u/69096827?v=4"
+          className="ml-auto"
+        />
+      </div>
     </header>
   );
 }
