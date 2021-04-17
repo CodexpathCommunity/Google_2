294f24a7ee417ce4e9d344c69b735350b44c2088
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-17 17:14:57 +0100

    started the server side rendering

---
+++ keys.js
@@ -0,0 +1,6 @@
+// API KEY - https://developers.google.com/custom-search/v1/using_rest
+// Context key - https://cse.google.com/cse/create/new
+
+export const API_KEY = "AIzaSyCfVr7AWAuelcu5b1uRrThEGe13ephJnPA";
+
+export const CONTEXT_KEY = "c509b17796a109017";
